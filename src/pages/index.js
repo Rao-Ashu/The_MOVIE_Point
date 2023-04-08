eabfa8bace2fdedfba8d31aca675c8703dae856b
Rao-Ashu <mit2021066@iiita.ac.in>
2023-04-08 18:03:29 +0530

    enhance homepage movie fetching

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,8 @@ import Head from 'next/head'
 
 
 export default function Home() {
+
+  const words = ["general", "blood", "shot", "social","fight","home", "city","own","star","dead", "house", "john"];
   return (
     <>
       <Head>
@@ -13,7 +15,7 @@ export default function Home() {
       <ThemeProvider enableSystem={false} attribute="class">
       <div className=' bg-gray-100 dark:bg-zinc-900 transition-all duration-900'>
         <Header />
-        <MovieList searchName={"general"}/>
+        <MovieList searchName={words[Math.floor(Math.random() * words.length)]}/>
       </div>
       </ThemeProvider>
       
